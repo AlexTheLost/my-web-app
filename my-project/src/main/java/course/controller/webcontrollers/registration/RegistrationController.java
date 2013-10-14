ef7bd19491ce0558c9303f666dad4dba0c558d85
Alex <nyaralex@gmail.com>
2013-10-14 14:08:54 +0300

    regist

--- my-project/src/main/java/course/controller/webcontrollers/registration/RegistrationController.java
+++ my-project/src/main/java/course/controller/webcontrollers/registration/RegistrationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,20 +13,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class RegistrationController {
 
     @Autowired
-    private RegistrationValidator  registrationValidator;
-    
+    private RegistrationValidator registrationValidator;
+
     @RequestMapping(method = RequestMethod.GET)
     public String registration(ModelMap model) {
         RegistrationForm registrationForm = new RegistrationForm();
         model.put("registrationForm", registrationForm);
         return "registration/registration";
     }
-    
-    @RequestMapping(method = RequestMethod.POST) 
-    public String processRegistration(RegistrationForm registrationForm,  BindingResult result) {
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String processRegistration(RegistrationForm registrationForm,
+            BindingResult result) {
         registrationValidator.validate(registrationForm, result);
-        if (result.hasErrors()) 
+        result = UserRegistrationHandler.isDataOccupied(result,
+                registrationForm.getUserName(), registrationForm.getPassword());
+        
+        if (result.hasErrors()) {
             return "registration/registration";
+        }
         return "registration/registration-success";
     }
 
---
+++ my-project/src/main/java/course/controller/webcontrollers/registration/UserRegistrationHandler.java
@@ -0,0 +1,25 @@
+package course.controller.webcontrollers.registration;
+
+import org.springframework.validation.BindingResult;
+
+import course.model.dao.users.UserDao;
+import course.model.dao.users.UserDaoImpl;
+import course.model.domain.users.User;
+
+public class UserRegistrationHandler {
+
+    public static BindingResult isDataOccupied(BindingResult result, String userName,
+            String email) {
+        User user = null;
+        UserDao userDao = new UserDaoImpl();
+        user = userDao.findByName(userName);
+        if (user != null) {
+            result.rejectValue("userName", "Username occupied.");
+        }
+        user = userDao.findByEmail(email);
+        if (user != null) {
+            result.rejectValue("email", "Email occupied.");
+        }
+        return result;
+    }
+}
